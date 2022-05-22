52cdb512debea62e9d9ae17a369042a7bb4d5db9
Кирилл <93085784+VolkodaV29@users.noreply.github.com>
2022-05-22 19:57:12 +0300

    Add files via upload

---
+++ lambda/src/Main.java
@@ -0,0 +1,24 @@
+public class Main {
+
+    public static void main(String[] args) {
+        Calculator calc = Calculator.instance.get();
+        int a = calc.plus.apply(1,2);
+        calc.printlin.accept(a);
+
+        int b = calc.minus.apply(2,1);
+        calc.printlin.accept(b);
+
+        int c = calc.devide.apply(a, b);
+        calc.printlin.accept(c);
+
+        int d = calc.pow.apply(a);
+        calc.printlin.accept(d);
+
+        int e = calc.abs.apply(-a);
+        calc.printlin.accept(a + e);
+
+        boolean check =  calc.isPositive.test(e);
+        System.out.println(check);
+
+    }
+}
